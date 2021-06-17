f05e8fb80c26b4acbe7dd2486b0bdfd3b8f097a9
AnSt <starininandrey@gmail.com>
2021-06-17 16:21:35 +0300

    Init

---
+++ ListBoxTemplateItem/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ListBoxTemplateItem
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> _users;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _users = new ObservableCollection<User>
+            {
+                new User("A", "123"),
+                new User("AA", "12"),
+                new User("AAA", "1"),
+            };
+            list_Users.ItemsSource = _users;
+        }
+
+        private void Button_Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            var login = input_Login.Text;
+            var password = input_Password.Text;
+        }
+    }
+}
---
+++ ListBoxTemplateItem/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ListBoxTemplateItem
+{
+    public class User
+    {
+        public string Login { get; init; }
+        public string Password { get; init; }
+
+        public User(string login, string password)
+        {
+            Login = login ?? throw new ArgumentNullException(nameof(login));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        public User() { }
+    }
+}
